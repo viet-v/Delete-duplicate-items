108869585c7a75a4fe03221e4aa6fd6efdfaa544
viet_v <vohoangviet459@gmail.com>
2021-10-21 22:44:29 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var groceries = [
+  "milk",
+  "coffee",
+  "oranges",
+  "yogourt",
+  "bread",
+  "juice",
+  "lettuce",
+  "flour",
+  "apples",
+  "milk",
+  "cat food",
+  "juice",
+  "ice cream",
+  "potatoes",
+  "bananas",
+  "coffee",
+  "olive oil",
+  "mustard"
+];
+// console.log(groceries.length);
+
+var deleteDuplicates = function (list) {
+  var cleanList = [];
+  for (var item of list) {
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    }
+  }
+  return cleanList;
+};
+
+var newGroceries = deleteDuplicates(groceries);
+console.log(newGroceries);
+
+newGroceries.forEach(function (item, index) {
+  console.log(`#${index + 1} ${item.toUpperCase()}`);
+});
